0750c41705109b6785c6dc12242fc158486c159b
pabloechez <pabloechez@gmail.com>
2018-05-19 18:50:06 +0200

    Update create_db.js

--- scripts/create_db.js
+++ scripts/create_db.js
@@ -1,12 +1,20 @@
 'use strict';
+require('dotenv').config();
 
 const MongooseSeed = require('mongoose-seed-db');
 const config = require('../lib/local_config');
 const path = require('path');
-var db= path.join(`${config.server}:${config.port}`,config.db);
+//var db= path.join(`${config.server}:${config.port}`,config.db);
 //import MongooseSeed from 'mongoose-seed-db'; //ES6
 
-MongooseSeed.connect('mongodb://'+db).then(() => {
+if(process.env.DATABASE_URI){
+    var db= process.env.DATABASE_URI;
+}else{
+    var db= path.join(`${config.server}:${config.port}`,config.db);
+    db='mongodb://'+ db;
+}
+
+MongooseSeed.connect(db).then(() => {
   MongooseSeed.loadModels(path.join(__dirname, '/../models'));
   MongooseSeed.clearAll().then(() => {
     MongooseSeed.populate(path.join(__dirname, '/../data')).then(() => {
